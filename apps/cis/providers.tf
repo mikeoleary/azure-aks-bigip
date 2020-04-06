5f55dc475e8c490548b8a43aa045e46e84cdc553
mikeoleary <michael.w.oleary@gmail.com>
2020-04-06 14:19:51 -0400

    k8s provider update

--- apps/cis/providers.tf
+++ apps/cis/providers.tf
@@ -5,6 +5,7 @@ provider "kubernetes" {
   client_certificate     = "${var.client_certificate}"
   client_key             = "${var.client_key}"
   cluster_ca_certificate = "${var.cluster_ca_certificate}"
+  load_config_file       = false
 }
 
 provider "helm" {
